aea95afef901d147daa4a421daa803ace9d51768
Ken Zhang <kyz211@nyu.edu>
2016-03-04 22:37:15 -0500

    remove dupes finished

--- linked-lists/remove-dups.py
+++ linked-lists/remove-dups.py
@@ -1,4 +1,4 @@
-#remove duplicates froma an unsorted linked list
+#remove duplicates from a an unsorted linked list
 class Node(object):
 
     def __init__(self, data=None, next_node=None):
@@ -14,4 +14,44 @@ class Node(object):
     def set_next(self, new_next):
         self.next_node = new_next
 
+six = Node(0, None)
+five = Node(1, six)
+four = Node(2, five)
+three = Node(3, four)
+two = Node(2, three)
+one = Node(1, two)
+head = Node(0, one)
 
+def print_linked_list(head):
+	if head != None:
+		print head.data
+	else:
+		print 'list does not exist'
+	while head.next_node != None:
+		head = head.next_node
+		print head.data
+print '--'
+print 'original list:'
+print_linked_list(head)
+print '--'
+
+value_set = set()
+def remove_dupes(head):
+	prev = None
+	if head == None:
+		print 'list does not exist'
+		return 0
+	while head != None:
+		if head.data in value_set:
+			prev.next_node = head.next_node
+			head = head.next_node
+		else:
+			value_set.add(head.data)
+			prev = head
+			head = head.next_node
+
+remove_dupes(head)
+print '--'
+print 'list with no dupes:'
+print_linked_list(head)
+print '--'
