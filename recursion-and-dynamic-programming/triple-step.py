2f1b9f88a3b8a9b98857b3d1564458d2b45cae84
Ken Zhang <kyz211@nyu.edu>
2016-03-14 02:11:46 -0400

    commit

--- recursion-and-dynamic-programming/triple-step.py
+++ recursion-and-dynamic-programming/triple-step.py
@@ -1,1 +1,13 @@
 #A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs
+count = {'count': 0}
+def countStairs(num):
+    if num < 0:
+        return
+    elif num == 0:
+        count['count'] += 1
+    countStairs(num - 1)
+    countStairs(num - 2)
+    countStairs(num - 3)
+
+countStairs(15)
+print(count['count'])
