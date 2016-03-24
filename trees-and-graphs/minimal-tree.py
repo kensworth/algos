0f84797b9eecc41dea0e47d36a13941df5b78057
Ken Zhang <kyz211@nyu.edu>
2016-03-24 19:18:23 -0400

    commit

---
+++ trees-and-graphs/minimal-tree.py
@@ -0,0 +1,5 @@
+#Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary tree with minimal height
+arr = [0,1,2,3,4,5,6,7,8,9,10]
+class binaryTree:
+    def __init__(self, arr):
+
