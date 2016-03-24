a69025619f7b48b7f936552494c3b6c86d2b6a6e
Ken Zhang <kyz211@nyu.edu>
2016-03-24 17:45:27 -0400

    commit

--- arrays-and-strings/is-permutation.py
+++ arrays-and-strings/is-permutation.py
@@ -1,3 +1,14 @@
 #write a method to determine if one string is a permutation of another
 
+str1 = 'hello'
+str2 = 'leloh'
+str3 = 'abcdefg'
+str4 = 'qwerty'
 
+def isPermutation(str1, str2):
+    if sorted(str1) == sorted(str2):
+        return True
+    return False
+
+print(isPermutation(str1, str2))
+print(isPermutation(str3, str4))
