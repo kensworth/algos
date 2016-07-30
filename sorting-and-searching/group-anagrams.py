3fb232cc20e26eb90b6cc1e5ae758d8372120da5
Ken Zhang <kyz211@nyu.edu>
2016-07-30 18:22:49 -0400

    finish group anagrams

--- sorting-and-searching/group-anagrams.py
+++ sorting-and-searching/group-anagrams.py
@@ -6,4 +6,19 @@ test1 = [
 ]
 
 def group_anagrams(arr):
+    unique_set = set()
+    unique_dictionary = {}
+    new_arr = []
+    for string in arr:
+        sorted_string = ''.join(sorted(string))
+        if sorted_string in unique_set:
+           unique_dictionary[sorted_string].append(string) 
+        else:
+            unique_set.add(sorted_string)
+            unique_dictionary[sorted_string] = [string]
+    for unique_string in unique_dictionary:
+        for anagram in unique_dictionary[unique_string]:
+            new_arr.append(anagram)
+    return new_arr
 
+print group_anagrams(test1)
