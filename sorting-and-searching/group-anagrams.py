26ab829a47cbe65f08855db3f2cb3c19ecbcc6e8
Ken Zhang <kyz211@nyu.edu>
2016-07-30 18:10:23 -0400

    add group anagrams problem

---
+++ sorting-and-searching/group-anagrams.py
@@ -0,0 +1,9 @@
+# Write a method to sort an array of strings so that all the anagrams are next to each other. 
+test1 = [
+    'abc', 'def', 'hij',
+    'bca', 'efd', 'ijh',
+    'cba', 'fed', 'jih'
+]
+
+def group_anagrams(arr):
+
